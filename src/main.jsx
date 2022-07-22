df5188f113b059280177de21325fcad7d84e24bb
edwin.martinez <EdwinSeb2014>
2022-07-21 21:52:01 -0500

    Redux Toolkit Query - cached objects

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import { store } from './store';
 import App from './App';
 import './index.css';
 import { PokemonApp } from './PokemonApp';
+import { TodoApp } from './TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store} >
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>
 )
